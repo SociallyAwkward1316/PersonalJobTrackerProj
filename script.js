45b016248cffda5c64649c93061c1fd3cd03bfd6
Alexis Fuenmayor <afuenmayor1316@gmail.com>
2026-04-02 14:40:46 -0500

    fixed on deletion bug and finished functionality

--- script.js
+++ script.js
@@ -25,17 +25,23 @@ const statusDetail = document.getElementById("statusDetail")
 const noteDetail = document.getElementById("noteDetail")
 
 const exitModalBtn = document.getElementById("exitModalBtn")
+const editInfoBtn = document.getElementById("editInfoBtn")
+const deleteBtn = document.getElementById("deleteBtn")
+
+let currentEditedRow = 0
 
 
 
 window.onload = function () {
-    const applications = JSON.parse(localStorage.getItem("applications"))
+    const applications = localStorage.getItem("applications")
 
     if (applications === null) {
         return
     }
 
-    applications.forEach(element => {
+    apps = JSON.parse(applications)
+
+    apps.forEach(element => {
         addRowToTable(element)
     });
 
@@ -45,6 +51,8 @@ window.onload = function () {
 function addRowToTable(application) {
     const tableBody = document.getElementById("tableBody")
     const tableRow = document.createElement("tr")
+
+    tableRow.dataset.id = application.id;
     tableRow.dataset.name = application.name;
     tableRow.dataset.url = application.url;
     tableRow.dataset.company = application.company;
@@ -68,6 +76,8 @@ function handleSubmit() {
     const status = statusInput.value
     const notes = jobNotes.value
 
+    const id = Date.now()
+
     if (!name || !url || !company || !position || !status) {
         alert("Fill out all fields");
         return;
@@ -77,17 +87,24 @@ function handleSubmit() {
 
     if (submissions === null) {
         const applications = []
-        applications.push({name: `${name}`, url: `${url}`, company: `${company}`, position: `${position}`, status: `${status}`, notes: `${notes}`})
+        applications.push({id:`${id}`, name: `${name}`, url: `${url}`, company: `${company}`, position: `${position}`, status: `${status}`, notes: `${notes}`})
         localStorage.setItem("applications", JSON.stringify(applications))
         
         addRowToTable(applications[0])
+
+        jobNameInput.value = ""
+        jobUrlInput.value = ""
+        companyInput.value = ""
+        positionInput.value = ""
+        statusInput.value = ""
+        jobNotes.value = ""
 
         return
     }
 
 
     const applications = JSON.parse(submissions)
-    const newApp = {name: `${name}`, url: `${url}`, company: `${company}`, position: `${position}`, status: `${status}`, notes: `${notes}`}
+    const newApp = {id:`${id}`, name: `${name}`, url: `${url}`, company: `${company}`, position: `${position}`, status: `${status}`, notes: `${notes}`}
     applications.push(newApp)
     localStorage.setItem("applications", JSON.stringify(applications))
     addRowToTable(newApp)
@@ -139,7 +156,7 @@ function companyInputFilter () {
         tableBody.innerHTML = ""
 
         filtered.forEach((element) => addRowToTable(element))
-    }   ``
+    }   
 }
 
 function handleClearBtn () {
@@ -160,6 +177,8 @@ function handleAppDetail (row) {
     companyDetail.textContent = row.dataset.company;
     positionDetail.textContent = row.dataset.position;
     statusDetail.textContent = row.dataset.status;
+    currentEditedRow = row.dataset.id
+
     if (row.dataset.notes !== "") {
         noteDetail.textContent = `${row.dataset.notes}`
     } else {
@@ -170,6 +189,105 @@ function handleAppDetail (row) {
 }
 
 function handleExitJobDetail () {
+    modalDiv.classList.toggle("detailActive")
+}
+
+function saveEdit () {
+    const name = document.querySelector("#jobNameDetail input").value;
+    const url = document.querySelector("#jobUrlDetail input").value;
+    const company = document.querySelector("#companyDetail input").value;
+    const position = document.querySelector("#positionDetail input").value;
+    const status = document.querySelector("#statusDetail select").value;
+    const note = document.querySelector("#noteDetail input").value;
+
+    jobNameDetail.textContent = name;
+    jobUrlDetail.textContent = url;
+    companyDetail.textContent = company;
+    positionDetail.textContent = position;
+    statusDetail.textContent = status;
+    noteDetail.textContent = note || "No notes yet.";
+
+    let applications = JSON.parse(localStorage.getItem("applications"))
+    const row = applications.find(app => app.id == currentEditedRow)
+    row.name = name
+    row.url = url
+    row.company = company
+    row.position = position
+    row.status = status
+    row.notes = note
+
+    const indexOfRow = applications.findIndex(app => app.id == currentEditedRow)
+
+    applications[indexOfRow] = row
+
+    localStorage.setItem("applications", JSON.stringify(applications))
+
+    const tableBody = document.getElementById("tableBody")
+    tableBody.innerHTML = ""
+
+    applications.forEach((app) => {
+        addRowToTable(app)
+    })
+
+    editInfoBtn.textContent = "Edit Info"
+    editInfoBtn.removeEventListener("click", saveEdit)
+    editInfoBtn.addEventListener("click", editInfoBtnClick)
+
+    
+
+
+    
+}
+
+function editInfoBtnClick () {
+    const nameSpan = document.getElementById("jobNameDetail")
+    const urlSpan = document.getElementById("jobUrlDetail")
+    const companySpan = document.getElementById("companyDetail")
+    const positionSpan = document.getElementById("positionDetail")
+    const statusSpan = document.getElementById("statusDetail")
+    const noteSpan = document.getElementById("noteDetail")
+
+    let name = nameSpan.textContent
+    let url = urlSpan.textContent
+    let company = companySpan.textContent
+    let position = positionSpan.textContent
+    let status = statusSpan.textContent
+    let note = noteSpan.textContent
+
+    nameSpan.innerHTML = `<input value="${name}">`;
+    urlSpan.innerHTML = `<input value="${url}">`;
+    companySpan.innerHTML = `<input value="${company}">`;
+    positionSpan.innerHTML = `<input value="${position}">`;
+    statusSpan.innerHTML = `
+    <select>
+        <option value="applied" ${status === "applied" ? "selected" : ""}>Applied</option>
+        <option value="interview" ${status === "interview" ? "selected" : ""}>Interview</option>
+        <option value="rejected" ${status === "rejected" ? "selected" : ""}>Rejected</option>
+        <option value="accepted" ${status === "accepted" ? "selected" : ""}>Accepted</option>
+    </select>`;
+    noteSpan.innerHTML = `<input value="${note}">`;
+
+    editInfoBtn.textContent = "Update Info"
+
+    editInfoBtn.removeEventListener("click", editInfoBtnClick)
+    editInfoBtn.addEventListener("click", saveEdit)
+
+}
+
+
+function handleDelete () {
+    let applications = JSON.parse(localStorage.getItem("applications"))
+    const appToDelete = applications.findIndex(app => app.id == currentEditedRow)
+
+    applications.splice(appToDelete, 1)
+    localStorage.setItem("applications", JSON.stringify(applications))
+
+    tableBody.innerHTML = ""
+    applications.forEach( (app) => {
+        addRowToTable(app)
+    }
+    )
+
     modalDiv.classList.toggle("detailActive")
 }
 
@@ -191,3 +309,5 @@ exitModalBtn.addEventListener("click", () => {
     modalDiv.classList.toggle("detailActive")
 })
 
+editInfoBtn.addEventListener("click", editInfoBtnClick)
+deleteBtn.addEventListener("click", handleDelete)
